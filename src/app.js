32ad42faf689ed24082c9a346e9e4a2b54866a06
Balaji <balaji.search87@gmail.com>
2025-02-05 23:28:27 +0530

    multiple routes in an array

--- src/app.js
+++ src/app.js
@@ -6,9 +6,31 @@ const app = express();
 
 //this will only handle GET call
 ///user/:userID/:name/:password - this is called dynamic routing
-app.get("/user/:userID/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Balaji", lastname:"Selvaraj"});
+// app.get("/user/:userID/:name/:password",(req,res)=>{
+//     console.log(req.params);
+//     res.send({firstname:"Balaji", lastname:"Selvaraj"});
+// });
+
+app.use("/user", 
+    
+  [  
+    (req, res, next)=>{
+    console.log("Routing Handler 1");
+    res.send("Response!!!");
+    next();
+},
+(req, res)=>{
+
+    console.log("Routing Handler 2");
+    res.send("Response 2");
+},
+  ],
+
+
+(req, res)=>{
+
+    console.log("Routing Handler 3");
+    res.send("Response 3");
 });
 
 
